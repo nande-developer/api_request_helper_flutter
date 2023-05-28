647bd859c985d8655777c2a9b9ee8a2246b44a2e
Fajar Septian Nugraha <fajar.nugraha@chargeplus.com>
2023-05-28 20:28:04 +0700

    feat(api_request_helper_flutter): add get request

--- lib/src/api_request_helper_flutter.dart
+++ lib/src/api_request_helper_flutter.dart
@@ -1,7 +1,66 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:http/http.dart' as http;
+
 /// {@template api_request_helper_flutter}
 /// Flutter package for handling http calls such as GET, POST, PUT, DELETE.
 /// {@endtemplate}
 class ApiRequestHelperFlutter {
   /// {@macro api_request_helper_flutter}
   const ApiRequestHelperFlutter();
+
+  /// Calls GET api which will emit [Future] Map<String, dynamic>
+  ///
+  /// Throws a [Exception] if response status code is not 200
+  Future<dynamic> get({
+    required Uri uri,
+    String? userToken,
+    Map<String, String>? body,
+  }) async {
+    final headers = {'Content-Type': 'application/json'};
+
+    if (userToken != null) {
+      headers.addAll({'Authorization': 'Bearer $userToken'});
+    }
+
+    log('ApiRequestHelper -- uri: $uri');
+    log('ApiRequestHelper -- request headers: $headers');
+
+    final request = http.Request('GET', uri);
+    request.headers.addAll(headers);
+
+    if (body != null) {
+      request.bodyFields = body;
+      log('ApiRequestHelper -- request body: $body');
+    }
+
+    return _sendRequest(request);
+  }
+
+  Future<dynamic> _sendRequest(http.BaseRequest request) async {
+    final response = await request.send().timeout(
+          const Duration(minutes: 1),
+          onTimeout: () => throw TimeoutException(''),
+        );
+
+    return _returnResponse(response);
+  }
+
+  Future<dynamic> _returnResponse(http.StreamedResponse response) async {
+    final stringBody = await response.stream.bytesToString();
+    final statusCode = response.statusCode;
+    final mappedResponse = jsonDecode(stringBody) as Map<String, dynamic>;
+    log('ApiRequestHelper -- response status code: $statusCode');
+    log('ApiRequestHelper -- body status code: $statusCode');
+    log('ApiRequestHelper -- body: $mappedResponse');
+
+    switch (mappedResponse['code']) {
+      case 200:
+        return stringBody;
+      default:
+        return Exception(mappedResponse['message']);
+    }
+  }
 }
